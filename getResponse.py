28f8739c7c85b5a687b721b44135b9720ce759b7
Pete Gordon <pete@petegordon.com>
2023-08-19 14:06:36 -0400

    added getResponse using llamaindex and ingest pointed to our own repo

--- getResponse.py
+++ getResponse.py
@@ -1,8 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+from llama_index import StorageContext, load_index_from_storage
+
 def getResponse(prompt):
-    return "Response: " + prompt
+    storage_context = StorageContext.from_defaults(persist_dir="index")
+
+    index = load_index_from_storage(storage_context)
+    query_engine = index.as_query_engine()
+    response = query_engine.query(prompt)
+    return response
 
 def main():
-    prompt = "Your question here"
+    prompt = "What is the API called for opening space ship doors?"
     result = getResponse(prompt)
     print(result)
 
